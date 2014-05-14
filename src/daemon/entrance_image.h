c28e179ddbef36349321909c6decb3f09aef4885
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2014-05-14 17:15:33 +0200

    Revert "entrance: added new functions for the user pool creation"
    This reverts commit 27c85e141c1d884c4f7b7146ce0b108d1b50b30e.

--- src/daemon/entrance_image.h
+++ src/daemon/entrance_image.h
@@ -5,7 +5,6 @@ Eina_List* entrance_image_system_icons();
 Eina_List* entrance_image_system_backgrounds();
 Eina_List* entrance_image_user_icons(const char *username);
 Eina_List* entrance_image_user_backgrounds(const char *username);
-Eina_List* entrance_image_user_pool_get(Eina_List *users);
-void entrance_image_user_pool_free(Eina_List *user_pool);
+
 
 #endif /* ENTRANCE_H_ */
