86c6986d9525626a1fcad461accbd3504a6548a8
William L. Thomson Jr <wlt@o-sinc.com>
2017-08-23 00:54:50 -0400

    utils: Updated entrance_xephyr.sh various improvements, fix for meson

--- utils/entrance_xephyr.sh
+++ utils/entrance_xephyr.sh
@@ -1,38 +1,57 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 DPI=${DPI:-142}
-SCREEN=${SCREEN:-640x480}
-VALGRIND=${VALGRIND:-0}
-GDB=${GDB:-0}
+SCREEN=${SCREEN:-1024x768}
 ENTRANCE=${ENTRANCE:-src/daemon/entrance}
+
+[[ -d build ]] && cd build
 
 if [[ ! -f src/daemon/entrance ]]; then
 	echo "src/daemon/entrance does not exist, run ./utils/local_build.sh"
 	exit 1
 fi
 
-while [ $# -gt 0 ]; do
-   case $1 in
-      -g|--gdb)
-         GDB=1
-         ;;
-      -v|--valgrind)
-         VALGRIND=1
-         ;;
-    esac
-    shift
+while :
+do
+	case "${1}" in
+		-d | --dpi)
+			DPI="${1}"
+			shift
+			;;
+		-e | --entrance)
+			ENTRANCE="${1}"
+			shift
+			;;
+		-g | --gdb)
+			GDB=1
+			;;
+		-s | --screen)
+			SCREEN="${1}"
+			shift
+			;;
+		-v | --valgrind)
+			VALGRIND=1
+			;;
+		*)
+			break
+			;;
+
+    	esac
 done
 
-
 #rm -f ~/.Xauthority
-Xephyr :1 -nolisten tcp -noreset -ac -br -dpi $DPI -screen $SCREEN &
+Xephyr :1 -nolisten tcp -noreset -ac -br -dpi "${DPI}" -screen "${SCREEN}" &
 sleep 1
-if [ $GDB -eq 1 ]
-then
-   gdb --args ${ENTRANCE} -x
-elif [ $VALGRIND -eq 1 ]
-then
-   valgrind --leak-check=full ${ENTRANCE} -x
-else
-   ${ENTRANCE} -x
-fi
 
+if [[ ${GDB} ]]; then
+	gdb --args "${ENTRANCE}" -x
+elif [[ ${VALGRIND} ]]; then
+	valgrind \
+		--leak-check=full \
+		--read-var-info=yes \
+		--show-reachable=yes \
+		--track-origins=yes \
+		"${ENTRANCE}" -x
+else
+	"${ENTRANCE}" -x
+fi
