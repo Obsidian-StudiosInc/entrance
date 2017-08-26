736a98c98e2b2009a4569e63f8c3ecc4d50f7cd8
William L. Thomson Jr <wlt@o-sinc.com>
2017-08-25 20:52:58 -0400

    build: Remove conf files from autotools makefile

--- src/bin/Makefile.mk
+++ src/bin/Makefile.mk
@@ -6,14 +6,6 @@ src/event/entrance_event.h \
 src/event/entrance_event.c \
 src/bin/entrance_client.h \
 src/bin/entrance_client.c \
-src/bin/entrance_conf.h \
-src/bin/entrance_conf.c \
-src/bin/entrance_conf_main.h \
-src/bin/entrance_conf_main.c \
-src/bin/entrance_conf_theme.h \
-src/bin/entrance_conf_theme.c \
-src/bin/entrance_conf_user.h \
-src/bin/entrance_conf_user.c \
 src/bin/entrance_connect.h \
 src/bin/entrance_connect.c \
 src/bin/entrance_fill.h \
@@ -30,3 +22,4 @@ src_bin_entrance_client_CPPFLAGS = \
 @ENTRANCE_CLIENT_CFLAGS@
 
 src_bin_entrance_client_LDADD = @ENTRANCE_CLIENT_LIBS@
+
