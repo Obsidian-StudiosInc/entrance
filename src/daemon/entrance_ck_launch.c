d31096f907afa00595215a94988dc45ba37707d2
William L. Thomson Jr <wlt@o-sinc.com>
2017-09-10 17:55:35 -0400

    daemon: Fix defect correct use of tainted untrusted string value, take 6

--- src/daemon/entrance_ck_launch.c
+++ src/daemon/entrance_ck_launch.c
@@ -42,7 +42,7 @@ main (int argc, char **argv)
        char exe[1024];
        int len;
        len = strlen(argv[1]);
-       if(len > 0 && len < 1024)
+       if(len > 0 && len < 1023)
          {
             snprintf(exe,len,"%s",argv[1]);
             char **args = (char **)malloc((argc-1) * sizeof(char *));
