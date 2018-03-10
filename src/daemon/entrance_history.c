Obsidian-StudiosInc/entrance

commit 4e1eac1e9d28d3ce6e17200f94615ac74fc3a918

daemon: Fix code smell, refactor avoid side effects of right hand operands

src/daemon/entrance_history.c
@@ -67,13 +67,16 @@ _entrance_history_read(void)
 
    ef = eet_open("/var/cache/"PACKAGE"/"ENTRANCE_HISTORY_FILE,
                  EET_FILE_MODE_READ_WRITE);
-   if (!(ef)
-       || !(_entrance_history = eet_data_read(ef, _eddh, ENTRANCE_SESSION_KEY)))
+   if (!ef)
      {
-        PT("Error on reading last session login");
-        _entrance_history = calloc(1, sizeof(Entrance_History));
-     }
-   eet_close(ef);
+       _entrance_history = eet_data_read(ef, _eddh, ENTRANCE_SESSION_KEY);
+       if(!_entrance_history)
+         {
+           PT("Error on reading last session login");
+           _entrance_history = calloc(1, sizeof(Entrance_History));
+         }
+     eet_close(ef);
+    }
 }
 
 static void

commit 2ec59eae6f3e1830dddef4315d5644e91da027cf

daemon: Fix bug introduced in 4e1eac1, invert initial conditional

src/daemon/entrance_history.c
@@ -67,7 +67,7 @@ _entrance_history_read(void)
 
    ef = eet_open("/var/cache/"PACKAGE"/"ENTRANCE_HISTORY_FILE,
                  EET_FILE_MODE_READ_WRITE);
-   if (!ef)
+   if (ef)
      {
        _entrance_history = eet_data_read(ef, _eddh, ENTRANCE_SESSION_KEY);
        if(!_entrance_history)
@@ -75,7 +75,7 @@ _entrance_history_read(void)
            PT("Error on reading last session login");
            _entrance_history = calloc(1, sizeof(Entrance_History));
          }
-     eet_close(ef);
+       eet_close(ef);
     }
 }
 