573117981970bf61dc4f5e7ad22f243aaef03fd6
William L. Thomson Jr <wlt@o-sinc.com>
2018-03-20 01:30:03 -0400

    tests: Create sample /usr/share/xsessions/Xsession.desktop

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # wrapper to run entrance with env vars
 
+
+
+[[ ! -d /usr/share/xsessions ]] && mkdir -p /usr/share/xsessions
+
+echo "[Desktop Entry]
+Name=XSession
+Comment=Xsession
+Exec=/etc/entrance/Xsession
+TryExec=/etc/entrance/Xsession
+" > /usr/share/xsessions/Xsession.desktop
+
 /usr/sbin/entrance
