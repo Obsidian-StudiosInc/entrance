ee66815e82925786b59eeb4c3899ab8c1f2e96be
William L. Thomson Jr <wlt@o-sinc.com>
2018-03-20 00:54:03 -0400

    tests: Drop printenv, unset, and touch commands, back to raw entrance

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # wrapper to run entrance with env vars
 
-printenv
-
-unset XAUTHORI{ZATION,TY}
-unset XDG_{SESSION_ID,RUNTIME_DIR}
-
-touch /var/run/entrance.auth
-
 /usr/sbin/entrance
