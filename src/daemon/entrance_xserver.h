803d9f6e4eb13ae77977f8f8487262737715f4d2
William L. Thomson Jr <wlt@o-sinc.com>
2018-03-20 01:22:25 -0400

    daemon: Change type from Eina_Bool to void, return value not used

--- src/daemon/entrance_xserver.h
+++ src/daemon/entrance_xserver.h
@@ -1,6 +1,6 @@
 #ifndef ENTRANCE_XLIB_H_
 #define ENTRANCE_XLIB_H_
-typedef Eina_Bool (*Entrance_X_Cb)(const char *data);
+typedef void (*Entrance_X_Cb)(const char *data);
 int entrance_xserver_init(Entrance_X_Cb start, const char *dname);
 void entrance_xserver_wait(void);
 void entrance_xserver_end(void);
