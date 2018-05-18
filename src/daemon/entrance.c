f51cfe246f4c3ff8b2fc89ae1133d8c6324e4ab2
William L. Thomson Jr <wlt@o-sinc.com>
2018-05-18 17:07:50 -0400

    daemon: Drop hard coded ENTRANCE_XEPHYR ":1.0", use xdisplay from conf
    Also drop setting ENTRANCE_XEPHYR env var which serves little use.
    Alternative means to -x flag for runnin under xephyr or something.

--- src/daemon/entrance.c
+++ src/daemon/entrance.c
@@ -8,7 +8,6 @@
 #include "Ecore_Getopt.h"
 #include <xcb/xcb.h>
 
-#define ENTRANCE_XEPHYR ":1.0"
 #define ENTRANCE_CONFIG_HOME_PATH "/var/cache/entrance/client"
 #define ENTRANCE_USER_MAX 33
 
@@ -447,9 +446,6 @@ main (int argc, char ** argv)
    eina_log_domain_level_set("entrance", 5);
    eina_log_domain_level_set("entrance_client", 5);
 
-   if (!_xephyr && getenv("ENTRANCE_XEPHYR"))
-     _xephyr = EINA_TRUE;
-
    eet_init();
    efreet_init();
    entrance_config_init();
@@ -460,13 +456,9 @@ main (int argc, char ** argv)
      }
 
    if (_xephyr)
-     {
-        nodaemon = EINA_TRUE;
-        entrance_display = strdup(ENTRANCE_XEPHYR);
-        putenv(strdup("ENTRANCE_XEPHYR=1"));
-     }
-   else
-     entrance_display = strdup(entrance_config->command.xdisplay);
+     nodaemon = EINA_TRUE;
+
+   entrance_display = strdup(entrance_config->command.xdisplay);
 
    if (!_xephyr && !_get_lock())
         exit(1);
