dc5142917380981dc657db568830a4602c8d3ae0
William L. Thomson Jr <wlt@o-sinc.com>
2018-07-09 22:06:52 -0400

    tests: Switch order of kill, kill server then client
    Client should already be stopped, but may need to be stopped in case of
    orphan. Though it should stop itself in that case. Stopping client first
    seems to cause some issue with changes in recent commits. Or they need
    to be reverted per issues in Travis.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -28,10 +28,10 @@ echo "${0} Going to sleep for ${SLEEP}"
 sleep ${SLEEP}
 echo "${0} Waking up"
 
-killall entrance_client
+kill ${EPID}
 
 sleep 5
 
-kill ${EPID}
+killall entrance_client
 
 sleep 5
