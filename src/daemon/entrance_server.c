b7738ebc574eaa69e34fa88fedafcda33c72b2bf
William L. Thomson Jr <wlt@o-sinc.com>
2019-09-25 20:12:29 -0400

    daemon: Send req_passwd from config file on server to client
    Feature requested in issue #35.
    https://github.com/Obsidian-StudiosInc/entrance/issues/35

--- src/daemon/entrance_server.c
+++ src/daemon/entrance_server.c
@@ -39,6 +39,7 @@ _entrance_server_add(void *data EINA_UNUSED, int type EINA_UNUSED, void *event E
         eev.event.conf_gui.enabled = EINA_TRUE;
         eev.event.conf_gui.bg.path = entrance_config->bg.path;
         eev.event.conf_gui.bg.group = entrance_config->bg.group;
+        eev.event.conf_gui.req_passwd = entrance_config->req_passwd;
         eev.event.conf_gui.theme = NULL;
         entrance_event_send(&eev);
      }
